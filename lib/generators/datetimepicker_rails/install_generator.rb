d5b1231cbea43b326421473eaad206690a60cfee
Jason <jasonZYJ@users.noreply.github.com>
2016-02-01 14:45:11 +0800

    Remove add assets script to root vendor folder

--- lib/generators/datetimepicker_rails/install_generator.rb
+++ lib/generators/datetimepicker_rails/install_generator.rb
@@ -57,73 +57,7 @@ module DatetimepickerRails
             File.exist?('config/initializers/ranged_datetime_wrapper.rb')
       end
 
-      def create_pickers_js
-        puts("icon_family: #{icon_family}")
-        icons = icon_family == 'Glyphicon' ? '' : get_icons
-
-        vendor 'assets/javascripts/pickers.js' do <<-FILE
-$(document).on('ready page:change', function() {
-  $('.datetimepicker').datetimepicker({
-    // put here your custom picker options, that should be applied for all pickers
-#{icons}
-  });
-
-  $('.datetimerange').each(function(){
-    var $this = $(this)
-    var range1 = $($this.find('.input-group')[0])
-    var range2 = $($this.find('.input-group')[1])
-
-    if(range1.data("DateTimePicker").date() != null)
-      range2.data("DateTimePicker").minDate(range1.data("DateTimePicker").date());
-
-    if(range2.data("DateTimePicker").date() != null)
-      range1.data("DateTimePicker").maxDate(range2.data("DateTimePicker").date());
-
-    range1.on("dp.change",function (e) {
-      if(e.date)
-        range2.data("DateTimePicker").minDate(e.date);
-      else
-        range2.data("DateTimePicker").minDate(false);
-    });
-
-    range2.on("dp.change",function (e) {
-      if(e.date)
-        range1.data("DateTimePicker").maxDate(e.date);
-      else
-        range1.data("DateTimePicker").maxDate(false);
-    });
-  })
-});
-        FILE
-        end
-      end
-
       # Source the submodule with git://github.com/eonasdan/bootstrap-datetimepicker.git
-      def copy_bootstrap_datetimepicker
-
-        # update the submodule to the latest source version
-        # git submodule: 'init'
-        # git submodule: 'update'
-
-        repo_root = File.join(File.expand_path("../../..", File.dirname(__FILE__)),
-                              "bootstrap-datetimepicker")
-
-        copy_file("#{repo_root}/build/css/bootstrap-datetimepicker.min.css",
-                  "vendor/assets/stylesheets/bootstrap-datetimepicker.min.css")
-
-        copy_file("#{repo_root}/build/css/bootstrap-datetimepicker.css",
-                  "vendor/assets/stylesheets/bootstrap-datetimepicker.css")
-
-        app_root = File.expand_path(File.dirname("."))
-        inside("#{repo_root}/src/js") do
-          file_paths = Dir["**/**/*"].select { |f| File.file?(f) }
-
-          file_paths.each do |file_path|
-            copy_file("#{repo_root}/src/js/#{file_path}", "#{app_root}/vendor/assets/javascripts/#{file_path}")
-          end
-        end
-
-      end
 
     private
 
